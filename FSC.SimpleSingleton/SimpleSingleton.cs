cd7b713b1f89733eee87f2f92be5c8bc85eeed06
Fabian2000 <35495166+Fabian2000@users.noreply.github.com>
2023-03-15 14:44:49 +0100

    Add files via upload

---
+++ FSC.SimpleSingleton/SimpleSingleton.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FSC.Singleton
+{
+    /// <summary>
+    /// Provides a simple thread-safe implementation of a singleton pattern.
+    /// </summary>
+    public static class SimpleSingleton
+    {
+        /// <summary>
+        /// A thread-safe collection to store lazy-initialized instances of objects.
+        /// </summary>
+        private static ConcurrentBag<Lazy<object>> _instances = new ConcurrentBag<Lazy<object>>();
+
+        /// <summary>
+        /// A lock object to synchronize access to the collection of instances.
+        /// </summary>
+        private static readonly object _lockObject = new object();
+
+        /// <summary>
+        /// Gets the singleton instance of a type, if it exists.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to get.</typeparam>
+        /// <returns>The singleton instance of the specified type, or <c>null</c> if it does not exist.</returns>
+        public static T? InstanceOf<T>()
+        {
+            lock (_lockObject)
+            {
+                var instance = _instances.FirstOrDefault(i => i.Value.GetType() == typeof(T));
+                return instance is null ? default(T) : (T)instance.Value;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether a singleton instance of a type exists.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to check.</typeparam>
+        /// <returns><c>true</c> if a singleton instance of the specified type exists; otherwise, <c>false</c>.</returns>
+        public static bool HasInstanceOf<T>()
+        {
+            T? instance = InstanceOf<T>();
+            return instance is not null;
+        }
+
+        /// <summary>
+        /// Registers a singleton instance of a type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to register.</typeparam>
+        /// <param name="instance">The singleton instance to register.</param>
+        /// <returns><c>true</c> if the instance was successfully registered; otherwise, <c>false</c>.</returns>
+        public static bool RegisterInstance<T>(T instance)
+        {
+            lock (_lockObject)
+            {
+                if (instance is null || HasInstanceOf<T>())
+                {
+                    return false;
+                }
+
+                _instances.Add(new Lazy<object>(() => instance));
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Unregisters a singleton instance of a type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to unregister.</typeparam>
+        /// <returns><c>true</c> if the instance was successfully unregistered; otherwise, <c>false</c>.</returns>
+        public static bool UnregisterInstanceOf<T>()
+        {
+            int initialCount;
+            lock (_lockObject)
+            {
+                initialCount = _instances.Count;
+                _instances = new ConcurrentBag<Lazy<object>>(_instances.Where(x => x.Value.GetType() != typeof(T)));
+            }
+            return initialCount != _instances.Count;
+        }
+
+        /// <summary>
+        /// Returns a list of all instances registered in the SimpleSingleton.
+        /// </summary>
+        /// <returns>A list of all instances.</returns>
+        public static List<object> GetAllInstances()
+        {
+            return _instances.Select(x => x.Value).ToList();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+using FSC.Singleton;
+
+namespace Test
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of StringBuilder and TestClass
+            StringBuilder sbInstance = new StringBuilder();
+            TestClass tcInstance = new TestClass();
+
+            // Register the instances
+            bool sbRegistered = SimpleSingleton.RegisterInstance(sbInstance);
+            bool tcRegistered = SimpleSingleton.RegisterInstance(tcInstance);
+
+            // Add the text "Hello World" to the StringBuilder instance
+            StringBuilder? sb = SimpleSingleton.InstanceOf<StringBuilder>();
+            bool sbAppended = sb?.Append("Hello World ") != null;
+
+            // Get the content of the StringBuilder and the number of TestClass
+            string sbContent = sb?.ToString() ?? string.Empty;
+            int tcNumber = SimpleSingleton.InstanceOf<TestClass>()?.Number ?? 0;
+
+            // Output the content and number
+            string output = $"{sbContent}{tcNumber}";
+            Console.WriteLine(output);
+
+            // Unregister the instances
+            bool sbUnregistered = SimpleSingleton.UnregisterInstanceOf<StringBuilder>();
+            bool tcUnregistered = SimpleSingleton.UnregisterInstanceOf<TestClass>();
+
+            // Output success or failure messages
+            Console.WriteLine(sbRegistered && tcRegistered ? "Instances successfully registered" : "Error registering instances");
+            Console.WriteLine(sbAppended ? "Text successfully appended" : "Error appending text");
+            Console.WriteLine(sbUnregistered && tcUnregistered ? "Instances successfully unregistered" : "Error unregistering instances");
+        }
+    }
+
+    internal class TestClass
+    {
+        internal int Number => 24;
+    }
+}
